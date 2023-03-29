e8f50301501ddff5a0af66df791d2954f3cb2a17
SamynathanS6720 <samynathan6720@gmail.com>
2023-03-29 11:24:31 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.16"
-#     }
-#   }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-#   required_version = ">= 1.2.0"
-# }
-# provider "aws" {
-#   region  = "ap-northeast-1"
-# }
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region  = "ap-northeast-1"
+}
 
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-030cf0a1edb8636ab"
-#   instance_type = "t2.micro"
+resource "aws_instance" "app_server" {
+  ami           = "ami-030cf0a1edb8636ab"
+  instance_type = "t2.micro"
 
-#   tags = {
-#     Name = "ExampleAppServerInstance"
-#   }
-# }
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
